5ca429d9cf0c13c188bf680078b9c99005d4be7c
oworayolam-dotcom <oworayolam@gmail.com>
2026-02-02 15:13:01 +0600

    Add files via upload

---
+++ StudentApplication.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package students.studentapplication;
+
+/**
+ * 
+ *
+ * @author YOLAM
+ */
+public class StudentApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
